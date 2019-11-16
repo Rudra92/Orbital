bd7e7aed955fc664b61c9460de8d381ce9db094d
Rodrigo Granja <rodrigo.soaresgranja@epfl.ch>
2019-11-17 00:04:53 +0100

    Implement player shooting with projectile instantiation and aim

--- Orbital-Stonks/Assets/PlayerBehaviour.cs
+++ Orbital-Stonks/Assets/PlayerBehaviour.cs
@@ -13,6 +13,12 @@ public class PlayerBehaviour : MonoBehaviour
     private GameObject[] planets;
     private GameObject currentPlanet;
 
+    private bool firing;
+    private float shootingAngle;
+    public GameObject aimCirclePrefab;
+    public GameObject projectilePrefab;
+    private GameObject aimCircle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +26,8 @@ public class PlayerBehaviour : MonoBehaviour
         this.rb2d = gameObject.GetComponent<Rigidbody2D>();
         this.planets = GameObject.FindGameObjectsWithTag(PLANET_TAG);
         this.currentPlanet = null;
+        this.firing = false;
+        shootingAngle = 0f;
     }
 
     // Update is called once per frame
@@ -27,6 +35,7 @@ public class PlayerBehaviour : MonoBehaviour
     {
         if (currentPlanet == null)
         {
+            // let it gravitate into a planet
             for (int i = 0; i < planets.Length; ++i)
             {
                 if (bc2d.IsTouching(planets[i].GetComponent<Collider2D>()))
@@ -39,17 +48,37 @@ public class PlayerBehaviour : MonoBehaviour
             }
         } else
         {
-            Vector3 planetPosition = currentPlanet.transform.position;
-            Vector3 planetToPlayer = transform.position - planetPosition;
+            if (firing)
+            {
+                if (Input.GetKeyDown("space"))
+                {
+                    Shoot();
+                }
+                PrepareShooting();
+            }
+            else
+            {
+                if (Input.GetKeyDown("f"))
+                {
+                    print("KEY DOWN");
+                    firing = true;
+                    shootingAngle = 0f;
+                    aimCircle = Instantiate(aimCirclePrefab);
+                    PrepareShooting();
+                }
+                else
+                {
+                    Vector3 planetPosition = currentPlanet.transform.position;
+                    Vector3 planetToPlayer = transform.position - planetPosition;
 
-            float angle = Mathf.Atan2(planetToPlayer.y, planetToPlayer.x);
-            float horiInput = 0.1f * Input.GetAxis("Horizontal") / currentPlanet.transform.localScale.x;
-            float distance = currentPlanet.transform.localScale.x / 2 + transform.localScale.y / 2;
+                    float angle = Mathf.Atan2(planetToPlayer.y, planetToPlayer.x);
+                    float horiInput = 0.1f * Input.GetAxis("Horizontal") / currentPlanet.transform.localScale.x;
+                    float distance = currentPlanet.transform.localScale.x / 2 + transform.localScale.y / 2;
 
-            transform.position = planetPosition + new Vector3(distance * Mathf.Cos(angle - horiInput), distance * Mathf.Sin(angle - horiInput), transform.position.z);
-            rb2d.velocity *= 0.9f;
-
-
+                    transform.position = planetPosition + new Vector3(distance * Mathf.Cos(angle - horiInput), distance * Mathf.Sin(angle - horiInput), transform.position.z);
+                    rb2d.velocity *= 0.9f;
+                }
+            }
 
             if (rb2d.velocity.magnitude <= MINIMAL_VELOCITY)
             {
@@ -75,8 +104,31 @@ public class PlayerBehaviour : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void PrepareShooting()
     {
-        
+        print(aimCircle.transform.position);
+        shootingAngle += 0.1f * Input.GetAxis("Horizontal");
+        shootingAngle = Mathf.Clamp(shootingAngle, -Mathf.PI / 2, Mathf.PI / 2);
+
+        Vector3 planetToPlayer = (transform.position - currentPlanet.transform.position).normalized;
+        float angle = Mathf.Atan2(planetToPlayer.y, planetToPlayer.x) - shootingAngle;
+        float distance = 2 * transform.localScale.x;
+
+        aimCircle.transform.position = transform.position + new Vector3(distance * Mathf.Cos(angle), distance * Mathf.Sin(angle), 0);
+    }
+
+    private void Shoot()
+    {
+        firing = false;
+        GameObject.Destroy(aimCircle);
+
+        Vector3 planetToPlayer = (transform.position - currentPlanet.transform.position).normalized;
+        float angle = Mathf.Atan2(planetToPlayer.y, planetToPlayer.x) - shootingAngle;
+        float distance = 2 * transform.localScale.x;
+
+        Vector3 projectilePosition = transform.position + new Vector3(distance * Mathf.Cos(angle), distance * Mathf.Sin(angle), 0);
+
+        GameObject projectile = Instantiate(projectilePrefab, projectilePosition, Quaternion.identity);
+        projectile.GetComponent<Rigidbody2D>().velocity = 5 * (projectilePosition - transform.position);
     }
 }
